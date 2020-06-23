3d8204d3135079d9f8130792530bfacdce6eb5c7
jpineda1 <jp@lightbend.com>
2020-06-22 18:06:57 -0700

    final styles and layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,6 @@ $(function() {
   scrollAdjust();
   navbarBurger();
 });
+
+function externalLinks() { 
+  for(var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) { var b = c[a]; b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();
